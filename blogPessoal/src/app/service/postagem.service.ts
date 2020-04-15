23e183dc5678daf29a6b3200e91016b11201c635
Gabriel Rodrigues de Oliveira <gabriel11.r@hotmail.com>
2020-04-15 14:46:56 -0300

    Criação do endPoint getAllPostagens

--- blogPessoal/src/app/service/postagem.service.ts
+++ blogPessoal/src/app/service/postagem.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostagemService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllPostagens(){
+    return this.http.get('http://31.220.57.14:8080/postagens')
+  }
+
 }

c1353e276fdfebac88476f301fda2d69c8819b0d
Gabriel Rodrigues de Oliveira <gabriel11.r@hotmail.com>
2020-04-22 16:57:28 -0300

    Implementação do getAllPostagens no componente Feed

--- blogPessoal/src/app/feed/feed.component.ts
+++ blogPessoal/src/app/feed/feed.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PostagemService } from '../service/postagem.service';
+import { Postagem } from '../model/Postagem';
 
 @Component({
   selector: 'app-feed',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedComponent implements OnInit {
 
-  constructor() { }
+  listaPostagens: Postagem[]
+
+  constructor(private postagemService: PostagemService) { }
 
   ngOnInit(): void {
+    this.findallPostagens()
   }
 
+  findallPostagens(){
+    this.postagemService.getAllPostagens().subscribe((resp: Postagem[])=>{
+      this.listaPostagens = resp
+    })
+  }
 }

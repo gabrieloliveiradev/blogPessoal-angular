8f2216496b89f85f1444a70656a8864937b275d2
Gabriel Rodrigues de Oliveira <gabriel11.r@hotmail.com>
2020-05-04 22:02:42 -0300

    Criação do componente editar e implementação do endpoint putPostagem

--- blogPessoal/src/app/app.module.ts
+++ blogPessoal/src/app/app.module.ts
@@ -10,7 +10,8 @@ import { FooterComponent } from './footer/footer.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from '@angular/common/http'
 import { FormsModule } from '@angular/forms'
-import { OrderModule } from 'ngx-order-pipe'
+import { OrderModule } from 'ngx-order-pipe';
+import { EditarComponent } from './editar/editar.component'
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { OrderModule } from 'ngx-order-pipe'
     HomeComponent,
     FeedComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    EditarComponent
   ],
   imports: [
     BrowserModule,
